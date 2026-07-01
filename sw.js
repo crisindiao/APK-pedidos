69a590e5999438431d98fe3db04986a44de761b8
crisindiao <139294558+crisindiao@users.noreply.github.com>
2026-07-01 18:37:20 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "imaginae-cache-v2";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./app.jsx"];
+const CACHE_NAME = "imaginae-cache-v3";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
